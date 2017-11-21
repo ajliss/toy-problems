413a1ffd37a49e58cdca4839e61b1ecdfbfc4243
ajliss <aaron@lissismore.com>
2017-11-21 13:00:36 -0800

    [problem 3] set up.

--- leetCode/problems.js
+++ leetCode/problems.js
@@ -58,7 +58,6 @@ Your function should return length = 2, with the first two elements of nums bein
  * @return {number}
  */
 
-
 const removeElement = (nums, val) => {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === val) {
@@ -67,4 +66,21 @@ const removeElement = (nums, val) => {
     }
   }
   return nums.length;
+};
+
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = (s) => {
+  
 };
